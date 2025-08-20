7102e720eccdc6fd8b9e1b7492bdafc58e818c85
Amira Gimeno <amiralorrainegimeno@gmail.com>
2025-08-20 13:50:38 +0800

    db connect

---
+++ includes/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+require 'config.php';
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
