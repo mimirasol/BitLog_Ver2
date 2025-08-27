1d4ae205b1cc8b954c22720761ebf13d5ac014c6
YordaSensei <nathsowner@gmail.com>
2025-08-27 19:49:24 +0800

    Added sidebar

--- views/dashboard.php
+++ views/dashboard.php
@@ -12,8 +12,11 @@ session_start();
       media="screen and (min-width: 1441px)">
 </head>
 <body>
-  <div class="container">
-    <div class="sidebar">
+  <div class="sidebar">
+    <div class="siderbar-items">
+      <button class="icon">
+        <img src="../css/assets/bitlog_coinanimation.gif" alt="Logo">
+      </button>
       <button class="icon">
         <img src="../css/assets/home_icon.png" alt="Home">
       </button>
@@ -23,15 +26,17 @@ session_start();
       <button class="icon">
         <img src="../css/assets/profile_icon.png" alt="Profile">
       </button>
-      <button class="icon">
-        <img src="../css/assets/logout_icon.png" alt="Logout">
-      </button>
     </div>
+  <button class="icon logout">
+    <img src="../css/assets/logout_icon.png" alt="Logout">
+  </button>
   </div>
 
-  <div class="main">
-    <h1>Dashboard Content</h1>
-    <p>This is where your dashboard content will appear.</p>
+  <div class="content">
+    <h2>Responsive Sidebar Example</h2>
+    <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+    <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+    <h3>Resize the browser window to see the effect.</h3>
   </div>
 
 </body>
